9c384b42d58cb8a0aa246647be5ef257b9e25f84
tawadarei <rei.1985.59@icloud.com>
2024-09-21 23:08:41 +0900

    練習問題12

---
+++ Practice12-1.java
@@ -0,0 +1,40 @@
+/*
+(1)    
+Sword
+Item
+Sword
+Item
+(2)
+Monster
+Slime
+Slime
+Monster
+正解でした！
+*/
+
+/*練習12−２
+ * ① a()   呼ぶ→箱
+ * 
+ * ②　AaBa   実際に動く中身→中身の型
+ *　　      　　改行してしまって間違えた
+ */
+
+
+ /*練習12−３
+  * ①Y[]型　　AとBはextends Y
+  *
+  * public class Main {
+  *   public static void main(String[] args) {
+  *     Y[] y = new Y[2];
+  *     y[0] = A();
+  *     y[1] = B();
+  *
+  *     for (Y cha : y) {
+  *       cha.b();
+  *
+  *     }
+  *   }
+  *}
+  *
+  *変数名が変だけど、あってる？と思う！
+  */
